566b5adc814d5717729611be256e96525697a60e
John Pasula <ackis81@gmail.com>
2017-10-30 12:21:58 -0600

    Pull the list of services from a file instead of hard coded in the script.  Same with the NMA Key.

--- checkmediaservers.sh
+++ checkmediaservers.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
-SERVICES=(
-	"ArchiSteamFarm"
-	"calibre"
-	"sabnzbdplus"
-	"minecraft"
-	"mylar"
-	"nginx"
-	"ombi"
-	"plexpy"
-	"plexmediaserver"
-	"radarr"
-	"sonarr"
-	"starbound"
-	"transmission"
-	"ubooquity"
-	"znc"
-#	"zoneminder"
-)
 
+# Notify My Android Script located here: http://www.notifymyandroid.com/dev.jsp
 
 INTROMESSAGE="Checking run state of media/game servers."
 RED="\033[0;31m"
 NC="\033[0m" # No Color
 GREEN="\033[0;32m"
-NMAKEY="adfd0c0ccc32ffc5d558307a71fc946ae4a921e6572e1c80"
 
-# Notify My Android Script located here: http://www.notifymyandroid.com/dev.jsp
+if [ ! -f services ]; then
+	SERVICES=(
+		"ArchiSteamFarm"
+		"calibre"
+		"sabnzbdplus"
+		"minecraft"
+		"mylar"
+		"nginx"
+		"ombi"
+		"plexpy"
+		"plexmediaserver"
+		"radarr"
+		"sonarr"
+		"starbound"
+		"transmission"
+		"ubooquity"
+		"znc"
+#		"zoneminder"
+	)
+else
+	readarray -t SERVICES < services
+fi
+
+if [ ! -f nmakey ]; then
+	echo "No NMA Key"
+	NMAKEY=""
+else
+	NMAKEY=$(head -n 1 nmakey)
+fi
 
 logger -t MediaServers -p syslog.info "${INTROMESSAGE}"
 echo "${INTROMESSAGE}"
@@ -38,7 +48,12 @@ for index in "${!SERVICES[@]}"; do
 	else
 		echo -e "${SERVICES[index]} running: ${RED}FAIL${NC}"
 		logger -p syslog.error "${SERVICES[index]} is NOT running."
-		/opt/scripts/notifymyandroid/nma.sh "MediaServers" "${SERVICES[index]}" "${SERVICES[index]} is not running." 2
+		if [ -z "$NMAKEY" ];
+		then
+			echo "No NMA key, skipping notification."
+		else
+			/opt/scripts/notifymyandroid/nma.sh "MediaServers" "${SERVICES[index]}" "${SERVICES[index]} is not running."
+		fi
 	fi
 
 done
