6eee603adda8b557a49abbcaa864ba432296c224
John Pasula <ackis81@gmail.com>
2018-02-08 07:59:10 -0700

    Lets add support for command line arguments.  Passing -v to the script will now allow you to log the "passes" to syslog instead of always doing it.

--- checkmediaservers.sh
+++ checkmediaservers.sh
@@ -7,8 +7,13 @@ RED="\\033[0;31m"
 NC="\\033[0m" # No Color
 GREEN="\\033[0;32m"
 
+# Display extra logging info
+VERBOSE=FALSE
+
 MY_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Read in the list of services from the services file.
+# If it doesn't exist, we'll just check for nginx
 if [ ! -f "${MY_PATH}/services" ]; then
 	SERVICES=(
 		"nginx"
@@ -17,6 +22,7 @@ else
 	readarray -t SERVICES < "${MY_PATH}/services"
 fi
 
+# Read in your Notify My Android key from the nmakey file.
 if [ ! -f "${MY_PATH}/nmakey" ]; then
 	echo "No NMA Key"
 	NMAKEY=""
@@ -24,17 +30,37 @@ else
 	NMAKEY=$(head -n 1 "${MY_PATH}/nmakey")
 fi
 
+while [ "$1" != "" ]; do
+	case $1 in
+		-v | --verbose )		shift
+						VERBOSE=TRUE
+						;;
+		-h | --help )			echo "Help"
+						exit
+						;;
+		* )				shift
+						VERBOSE=FALSE
+	esac
+	shift
+done
+
 logger -t MediaServers -p syslog.info "${INTROMESSAGE}"
 echo "${INTROMESSAGE}"
 
 for index in "${!SERVICES[@]}"; do
 	if (( $(ps -ef | grep -v grep | grep -c "${SERVICES[index]}") > 0 ))
 	then
+		# Print out the service with a green pass.
 		echo -e "${SERVICES[index]} running: ${GREEN}PASS${NC}"
-		logger -t MediaServers -p syslog.notice "${SERVICES[index]} is running."
+		# If we've selected verbose output, log the passing services to syslog.
+		if [ "$VERBOSE" = TRUE ] ; then
+			logger -t MediaServers -p syslog.notice "${SERVICES[index]} is running."
+		fi
 	else
+		# Print out the service with a red fail beside it, and then log it to our syslog error log.
 		echo -e "${SERVICES[index]} running: ${RED}FAIL${NC}"
 		logger -p syslog.error "${SERVICES[index]} is NOT running."
+		# If we have a Notify My Android key, send a notification.
 		if [ -z "$NMAKEY" ];
 		then
 			echo "No NMA key, skipping notification."
